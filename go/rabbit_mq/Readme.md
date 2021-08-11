0b7e0e843160446549dc3434b5641138ca4082ee
shashidhar <shashidhar@amagi.com>
2021-08-11 21:48:21 +0530

    golang rabbitmq usage

---
+++ go/rabbit_mq/Readme.md
@@ -0,0 +1,20 @@
+# Running producer and consumer through rabbitmq
+
+**Run the following on terminal 1**
+
+```
+rabbitmq-server
+```
+
+**On terminal 2**
+
+```
+go run consumer.go
+```
+
+**On terminal 3**
+
+```
+go run producer.go
+```
+
---
+++ go/rabbit_mq/consumer.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	fmt.Println("Go RabbitMQ Tutorial")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		fmt.Println("Failed Initializing Broker Connection")
+		panic(err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer ch.Close()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	q, err := ch.QueueDeclare(
+		"TestQueue", // name
+		false,       // durable
+		false,       // delete when unused
+		false,       // exclusive
+		false,       // no-wait
+		nil,         // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	ch.QueueBind("TestQueue", "TestQueue", "amq.direct", false, nil)
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	forever := make(chan bool)
+	go func() {
+		for d := range msgs {
+			fmt.Printf("Recieved Message: %s\n", d.Body)
+		}
+	}()
+
+	fmt.Println("Successfully Connected to our RabbitMQ Instance")
+	fmt.Println(" [*] - Waiting for messages")
+	<-forever
+}
